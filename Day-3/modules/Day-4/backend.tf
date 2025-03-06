cce4067fc61fbb06efe03a0bf212b4c2ee3c22bd
omgarmode <158752149+omtechops@users.noreply.github.com>
2025-03-06 09:33:51 +0530

    Delete Day-3/modules/Day-4 directory

--- Day-3/modules/Day-4/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devops-demo-xyz" # change this
-    key            = "om/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
