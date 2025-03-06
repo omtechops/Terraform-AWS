32fe4e6d3ae72b5ae8021961c0de707b1b00007f
omgarmode <158752149+omtechops@users.noreply.github.com>
2025-03-06 09:31:04 +0530

    Create variables.tf

---
+++ Day-3/modules/ec2_instance/variables.tf
@@ -0,0 +1,15 @@
+variable "ami_value" {
+  description = "value for my ami"
+}
+
+variable "instance_type_value" {
+  description = "value for my instance_type_value"
+}
+
+variable "subnet_id_value" {
+  description = "value for subnet id"
+}
+
+variable "key_name_value" {
+  description = "value for my keypair"
+}
